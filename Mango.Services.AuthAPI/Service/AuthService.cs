10c736820437a2b0f3fcdedc39616a6c364c34a4
Pan Maolin <enscausasui@outlook.com>
2025-10-23 07:49:51 +0800

    Create register endpoint

---
+++ Mango.Services.AuthAPI/Service/AuthService.cs
@@ -0,0 +1,75 @@
+﻿using Mango.Services.AuthAPI.Data;
+using Mango.Services.AuthAPI.Models;
+using Mango.Services.AuthAPI.Models.Dto;
+using Mango.Services.AuthAPI.Service.IService;
+
+using Microsoft.AspNetCore.Identity;
+
+namespace Mango.Services.AuthAPI.Service;
+
+public class AuthService : IAuthService
+{
+    private readonly AppDbContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+    public AuthService(AppDbContext db, UserManager<ApplicationUser> userManager,
+        RoleManager<IdentityRole> roleManager)
+    {
+        _db = db;
+        _userManager = userManager;
+        _roleManager = roleManager;
+    }
+
+    public Task<bool> AssignRole(string email, string roleName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<string> Register(RegistrationRequestDto registrationRequestDto)
+    {
+        //var userExists = await _userManager.FindByEmailAsync(registrationRequestDto.Email);
+        //if (userExists != null)
+        //    return "User already exists";
+        ApplicationUser user = new()
+        {
+            UserName = registrationRequestDto.Email,
+            Email = registrationRequestDto.Email,
+            //SecurityStamp = Guid.NewGuid().ToString(),
+            NormalizedEmail = registrationRequestDto.Email.ToUpper(),
+            Name = registrationRequestDto.Name,
+            PhoneNumber = registrationRequestDto.PhoneNumber
+        };
+        try
+        {
+            var result = await _userManager.CreateAsync(user, registrationRequestDto.Password);
+            if (result.Succeeded)
+            {
+                //var userToReturn = await _userManager.FindByEmailAsync(registrationRequestDto.Email);
+                var userToReturn = _db.ApplicationUsers.First(u => u.UserName == registrationRequestDto.Email);
+                UserDto userDto = new()
+                {
+                    Email = userToReturn.Email,
+                    ID = userToReturn.Id,
+                    Name = userToReturn.Name,
+                    PhoneNumber = userToReturn.PhoneNumber
+                };
+                //return System.Text.Json.JsonSerializer.Serialize(userDto);
+                return "";
+            }
+            else
+            {
+                return result.Errors.First().Description;
+            }
+        }
+        catch (Exception ex)
+        {
+            var msg = ex.Message;
+            return msg;
+        }
+    }
+}
